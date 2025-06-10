2f29b590b196f630bf356dd338d1a7c6aa909eba
sudocbrown99 <cmichaelbrown99@gmail.com>
2025-06-10 15:05:14 -0500

    Finished Ch1 L6

--- main.py
+++ main.py
@@ -2,26 +2,49 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+
 
 def main():
     load_dotenv()
-    api_key = os.environ.get("GEMINI_API_KEY")
 
-    client = genai.Client(api_key=api_key)
+    args = sys.argv[1:]
+    verbose = "--verbose" in args
+    filtered_args = [arg for arg in args if arg != "--verbose"]
 
-    if len(sys.argv) == 1:
-        print("Must provide an input")
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here"')
+        print('Example: python main.py "How do I build a calculator app?"')
         sys.exit(1)
 
-    user_input = sys.argv[1]
-    
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    user_prompt = " ".join(filtered_args)
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    generate_content(client, messages, verbose)
+
+def generate_content(client, messages, verbose=False):
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents=user_input
-    )   
-    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-    print("Response tokens:", response.usage_metadata.candidates_token_count)
-    print("Response:")
-    print(response.text)
+    model="gemini-2.0-flash-001",
+    contents=messages,
+    )
+    
+    if verbose == True:
+        print("User prompt:")
+        print(response.text)
+        print("Prompt tokens:")
+        print(response.usage_metadata.prompt_token_count)
+        print("Response tokens:")
+        print(response.usage_metadata.candidates_token_count)
+    else:
+        print("Response:")
+        print(response.text)
 
 
 if __name__ == "__main__":
