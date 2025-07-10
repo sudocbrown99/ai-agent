7ac91e4b80916f2cac598814bdc3286d91778a60
sudocbrown99 <cmichaelbrown99@gmail.com>
2025-07-10 16:51:35 -0500

    Working through CH2 L2

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -6,5 +6,19 @@ def get_files_info(working_directory, directory=None):
     if abs_dir.startswith(abs_working) == False:
         return (f'Error: Cannot list "{directory}" as it is outside the permitted working directory')
     
-    if os.path.isdir(directory) == False:
+    if os.path.isdir(abs_dir) == False:
         return (f'Error: "{directory}" is not a directory')
+    
+    dir_list = os.listdir(abs_dir)
+
+    final_result = []
+
+    for item in dir_list:
+        item_path = os.path.join(abs_dir, item)
+
+        file_size = os.path.getsize(item_path)
+        dir_status = os.path.isdir(item_path)
+        
+        final_result.append(f"- {item}: file_size={file_size} bytes, is_dir={dir_status}")
+
+    return "\n".join(final_result)
