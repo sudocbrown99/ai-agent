7877a724644579dca4c4aed5b0dbcbd8f17ea307
sudocbrown99 <cmichaelbrown99@gmail.com>
2025-07-15 17:56:16 -0500

    Finished Ch2 L2

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,24 +1,34 @@
-def get_files_info(working_directory, directory=None):
+import os
+
+def get_files_info(working_directory, directory="."):
     
     abs_working = os.path.abspath(working_directory)
     abs_dir = os.path.abspath(os.path.join(working_directory, directory))
     
-    if abs_dir.startswith(abs_working) == False:
+    if os.path.commonpath([abs_working, abs_dir]) != abs_working:
         return (f'Error: Cannot list "{directory}" as it is outside the permitted working directory')
     
     if os.path.isdir(abs_dir) == False:
         return (f'Error: "{directory}" is not a directory')
     
-    dir_list = os.listdir(abs_dir)
+    try:
+        dir_list = os.listdir(abs_dir)
+    except Exception as e:
+        return f"Error: {str(e)}"
 
     final_result = []
 
     for item in dir_list:
         item_path = os.path.join(abs_dir, item)
-
-        file_size = os.path.getsize(item_path)
-        dir_status = os.path.isdir(item_path)
+        try:
+            file_size = os.path.getsize(item_path)
+            dir_status = os.path.isdir(item_path)
+            final_result.append(f"- {item}: file_size={file_size} bytes, is_dir={dir_status}")
+        except Exception as e:
+            return f"Error:{str(e)}"
         
-        final_result.append(f"- {item}: file_size={file_size} bytes, is_dir={dir_status}")
+
 
     return "\n".join(final_result)
+
+    
