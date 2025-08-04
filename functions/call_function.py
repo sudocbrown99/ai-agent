46a08f5c0596b3b19af547e582d1f48eb1acd8af
sudocbrown99 <cmichaelbrown99@gmail.com>
2025-08-04 17:01:50 -0500

    Started Ch3 L4

---
+++ functions/call_function.py
@@ -0,0 +1,25 @@
+from google.genai import types
+from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
+from functions.run_python import run_python_file
+from functions.write_file import write_file
+
+available_functions = {
+    "get_file_content": get_file_content,
+    "get_files_info": get_files_info,
+    "run_python_file": run_python_file,
+    "write_file": write_file,
+}
+
+def call_function(function_call_part, verbose=False):
+
+    if verbose:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+    else:
+        print(f" - Calling function: {function_call_part.name}")
+
+    function_to_call = available_functions[function_call_part.name]
+
+    result = function_to_call(**function_call_part.args, working_directory="./calculator")
+
+    return result
